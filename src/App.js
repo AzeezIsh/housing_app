417fd1f83903fa5047cf123be39ee3f3b4ddc30c
Faiq Ahmad <faiqahmadpk@gmail.com>
2023-04-15 20:27:11 -0400

    basic react app plus some about page stuff being tested

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import neighborhood from './assets/neighborhood.jpeg'
+
+function App() {
+  return (
+    <div>
+      <h1>
+        This is a Test!
+      </h1>
+      <img src={neighborhood} alt='neighborhood'></img>
+    </div>
+  )
+}
+
+export default App;
